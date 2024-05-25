d2665256460116e03e51094aa27d8e454b85c438
Brandon Jordan <brandonjordan124@gmail.com>
2024-05-25 11:41:32 -0400

    Try to fix deployment

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     plugins: [vue()],
     base: '/shortcuts-glyph-search/',
+    esbuild: {
+        target: 'esnext',
+        platform: 'linux',
+    }
 })
