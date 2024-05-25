a41ecb6f97ee0e1c4defc80e606397bad7705201
Brandon Jordan <brandonjordan124@gmail.com>
2024-05-25 11:57:04 -0400

    Update deployment

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,4 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     plugins: [vue()],
     base: '/shortcuts-glyph-search/',
-    esbuild: {
-        target: 'esnext',
-        platform: 'linux',
-    }
 })
